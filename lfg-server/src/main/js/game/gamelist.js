4e79dd11bf47b70b40a735de5704c903fc5e20bf
GhostOfPQ <ghostofpq@Gmail.com>
2017-10-16 17:59:51 +0200

    games list

--- lfg-server/src/main/js/game/gamelist.js
+++ lfg-server/src/main/js/game/gamelist.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
 class GameList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {games: []};
+	}
+
+	componentDidMount() {
+		fetch("http://localhost:8080/api/games")
+		.then(result=>result.json())
+		.then(games=>
+			{	
+				games.forEach(game=>this.addGameInGameList(game));
+			})
+	}
+
+
+	addGameInGameList(game){
+		this.setState(prevState => ({
+			games: [...prevState.games, game]
+		}))
+	}
+
 	render() {
-		return (<h1>IT S GAMEZZZZ</h1>)
+		var nameList = this.state.games.map(
+			(game,index)=>{
+				var link = "#/games/"+game.id;
+				return (
+					<a key={index} href={link}>{game.name}</a>
+				);
+			}
+		)
+		console.log(nameList);
+		return (
+			<div>
+				{nameList}
+			</div>
+		)
 	}
 };
+
 export default GameList;
