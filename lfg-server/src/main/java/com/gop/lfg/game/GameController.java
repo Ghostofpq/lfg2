ff1a9308a7501c76fcb2ec03b208f32329d58317
GhostOfPQ <ghostofpq@Gmail.com>
2017-10-16 16:56:39 +0200

    games and game react routes

--- lfg-server/src/main/java/com/gop/lfg/game/GameController.java
+++ lfg-server/src/main/java/com/gop/lfg/game/GameController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController()
 public class GameController {
     private GameService gameService;
 
@@ -16,13 +16,13 @@ public class GameController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(path = "/games", method = RequestMethod.POST)
+    @RequestMapping(path = "/api/games", method = RequestMethod.POST)
     public GameDTO createGame(@RequestBody final Game gameCreationRequest) throws GameAlreadyExistsException {
         return gameService.create(gameCreationRequest);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(path = "/games", method = RequestMethod.GET)
+    @RequestMapping(path = "/api/games", method = RequestMethod.GET)
     public List<GameDTO> findGames(@RequestParam(name = "name", defaultValue = "") final String name,
                                    @RequestParam(name = "page", defaultValue = "0") final int page,
                                    @RequestParam(name = "size", defaultValue = "10") final int size) throws GameNotFoundException {
@@ -30,19 +30,19 @@ public class GameController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(path = "/game/{gameId}", method = RequestMethod.GET)
+    @RequestMapping(path = "/api/game/{gameId}", method = RequestMethod.GET)
     public GameDTO getGame(@PathVariable("gameId") final String id) throws GameNotFoundException {
         return gameService.findById(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(path = "/game/{gameId}", method = RequestMethod.PUT)
+    @RequestMapping(path = "/api/game/{gameId}", method = RequestMethod.PUT)
     public GameDTO updateGame(@PathVariable("gameId") final String id, @RequestBody final Game gameUpdateRequest) throws GameNotFoundException {
         return gameService.update(id, gameUpdateRequest);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @RequestMapping(path = "/game/{gameId}", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/api/game/{gameId}", method = RequestMethod.DELETE)
     public void deleteGame(@PathVariable("gameId") final String id) throws GameNotFoundException {
         gameService.deleteById(id);
     }
--- lfg-server/src/main/java/com/gop/lfg/security/WebSecurityConfig.java
+++ lfg-server/src/main/java/com/gop/lfg/security/WebSecurityConfig.java
@@ -13,13 +13,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/*").permitAll();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .antMatchers("/**/*").permitAll();
     }
 }
